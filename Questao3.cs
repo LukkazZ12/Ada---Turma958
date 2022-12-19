f1487a4efa2390738dc00827806f80122889f6c1
Lucas de S. Santos <110189896+LukkazZ12@users.noreply.github.com>
2022-12-19 16:40:15 -0300

    Subi a questão 3.

---
+++ Questao3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questoes123
+{
+    internal class Questao3
+    {
+    }
+}
