601adef36fba23647a267e0a0e10c475091746f3
Lucas de S. Santos <110189896+LukkazZ12@users.noreply.github.com>
2022-12-20 21:57:35 -0300

    Update Questao2.cs

--- Questao2.cs
+++ Questao2.cs
@@ -4,50 +4,63 @@ namespace Questoes123
 {
     internal class Questao2
     {
-        static void Main(string[] args)
+        static void Mainddd(string[] args)
         {
             bool conversao = true;
             string caminhoDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            string caminhoMatriz = Path.Combine(caminhoDesktop, "matriz.txt");
-            string caminhoCaminho = Path.Combine(caminhoDesktop, "caminho.txt");
-            string matriz = System.IO.File.ReadAllText(caminhoMatriz);
-            string caminho = System.IO.File.ReadAllText(caminhoCaminho);
-            string[] submatriz = matriz.Split('\n');
-            int i = submatriz.Length;
-            int[,] submatriz2 = new int[i, i];
-            for (int j = 0; j < i; j++)
+            var caminhoMatriz = new FileInfo(Path.Combine(caminhoDesktop, "matriz.txt"));
+            var caminhoCaminho = new FileInfo(Path.Combine(caminhoDesktop, "caminho.txt"));
+            if (!caminhoMatriz.Exists || !caminhoCaminho.Exists)
             {
-                string[] subaux = submatriz[j].Split(',');
-                if (subaux.Length != i)
+                Console.WriteLine("O arquivo matrix.txt ou caminho.txt não existe no Desktop.");
+                return;
+            }
+            string matriz = System.IO.File.ReadAllText(caminhoMatriz.FullName);
+            string caminho = System.IO.File.ReadAllText(caminhoCaminho.FullName);
+            if (matriz == "" || caminho == "")
+            {
+                Console.WriteLine("Não foi possível ler o arquivo matriz.txt ou caminho.txt.");
+                return;
+            }
+            string[] submatriz = matriz.Split(Environment.NewLine);
+            if (submatriz.Length <= 1)
+            {
+                Console.WriteLine("A quantidade de cidades é inválida.");
+                return;
+            }
+            int[,] submatriz2 = new int[submatriz.Length, submatriz.Length];
+            for (int i = 0; i < submatriz.Length; i++)
+            {
+                string[] subaux = submatriz[i].Split(',');
+                if (subaux.Length != submatriz.Length)
                 {
                     Console.WriteLine("A matriz não é quadrada.");
                     return;
                 }
-                for (int k = 0; k < i; k++)
+                for (int j = 0; j < submatriz.Length; j++)
                 {
-                    conversao = int.TryParse(subaux[k], out submatriz2[j, k]);
-                    if (!conversao || submatriz2[j, k] < 0 || (j != k && submatriz2[j, k] == 0) || (j == k && submatriz2[j, k] != 0))
+                    conversao = int.TryParse(subaux[j], out submatriz2[i, j]);
+                    if (!conversao || submatriz2[i, j] < 0 || (i != j && submatriz2[i, j] == 0) || (i == j && submatriz2[i, j] != 0))
                     {
                         Console.WriteLine("Há uma distância inválida na matriz.");
                         return;
                     }
-                }  
+                }
             }
             string[] subcaminho = caminho.Split(',');
-            int n = subcaminho.Length;
-            int[] subcaminho2 = new int[n];
-            for (int l = 0; l < n; l++)
+            int[] subcaminho2 = new int[subcaminho.Length];
+            for (int i = 0; i < subcaminho.Length; i++)
             {
-                conversao = int.TryParse(subcaminho[l], out subcaminho2[l]);
-                if (!conversao || subcaminho2[l] <= 0 || subcaminho2[l] > i)
+                int.TryParse(subcaminho[i], out subcaminho2[i]);
+                if (subcaminho2[i] <= 0 || subcaminho2[i] > submatriz.Length)
                 {
                     Console.WriteLine("Há uma cidade inválida no caminho.");
                     return;
                 }
             }
             int distanciaTotal = 0;
-            for (int m = 0; m < n - 1; m++)
-                distanciaTotal += submatriz2[subcaminho2[m] - 1, subcaminho2[m + 1] - 1];
+            for (int i = 0; i < subcaminho.Length - 1; i++)
+                distanciaTotal += submatriz2[subcaminho2[i] - 1, subcaminho2[i + 1] - 1];
             Console.WriteLine($"A distância total do percurso é {distanciaTotal} km.");
         }
     }
